c0790f71ffe916d6fcde9cf279f65f31bc522c4e
caique325 <134814288+caique325@users.noreply.github.com>
2023-09-18 10:12:54 -0300

    Create pessoa.py

---
+++ pessoa.py
@@ -0,0 +1,14 @@
+class Pessoa:
+    pass
+
+if __name__ == '__main__':
+
+    pessoa = Pessoa();
+
+    #setattr(pessoa,'nome','Tiago da Silva Curtinas');
+    setattr(pessoa, 'idade',39);
+    
+
+    #print(pessoa.nome);
+    print(pessoa.idade)
+    
